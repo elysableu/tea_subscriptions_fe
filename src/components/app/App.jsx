69978f32f17e2b0ca3030d7d651d2ecf1add9c6d
elysableu <elysabward@gmail.com>
2025-03-11 11:44:06 -0600

    Set up component files

---
+++ src/components/app/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <main>
+      <Header>
+        <h1>Bloom & Brew</h1>
+        <NavLink></NavLink>
+      </Header>
+
+      <Routes>
+        {/* <Route path="/" element={Homepage}></Route> */}
+      </Routes>
+    </main>
+  )
+}
+
+export default App
