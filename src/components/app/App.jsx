acddbd058eb23e64e74aba043539c11d4c4ceba5
elysableu <elysabward@gmail.com>
2025-03-11 13:51:42 -0600

    Set up homepage and router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,21 +1,28 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Header from '../Header/Header';
+import Homepage from '../homepage/Homepage';
+
 import './App.css'
 
 function App() {
+  const [subscriptions, setSubscriptions] = useState({});
+  cosnt [details, setDetails] = useState({});
   
 
   return (
     <main>
-      <Header>
-        <h1>Bloom & Brew</h1>
-        <NavLink></NavLink>
-      </Header>
+      <Header />
 
-      <Routes>
-        {/* <Route path="/" element={Homepage}></Route> */}
-      </Routes>
+      <section className="content">
+        <Routes>
+          <Route path="/" element={<Homepage subscriptions={subscriptions}/>} />
+
+        </Routes>
+      </section>
+      
     </main>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import './Homepage.css';
+
+function Homepage() {
+
+    return (
+        <section className="homepage-container">
+            <h2>Welcome to the admin portal!</h2>
+            <button>Enter portal!</button>
+        </section>
+    );
+}
+
+export default Homepage;
