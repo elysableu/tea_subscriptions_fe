e229ea5c8d9a52039622049f7acf404015e40c29
elysableu <elysabward@gmail.com>
2025-03-12 20:03:17 -0600

    Update cards css

--- src/components/subscriptionCard/SubscriptionCard.jsx
+++ src/components/subscriptionCard/SubscriptionCard.jsx
@@ -1,9 +1,13 @@
 import './SubscriptionCard.css';
+import Teapot from '../../assets/coffee-tea-kettle-icon.svg'
 
 function SubscriptionCard({ id, title, price, status, frequency, getSubscriptionDetails }) {
     return(
         <section className="subscription_card_container" onClick={() => getSubscriptionDetails(id)}>
-            <h3>{ title }</h3>
+            <div>
+                <img src={Teapot}/>
+                <h3>{ title }</h3>
+            </div>
             <div className="sub_card_info">
                 <p>Price per Box: ${ price }</p>
                 <p>Delivered { frequency }</p>
--- src/components/teaCard/TeaCard.jsx
+++ src/components/teaCard/TeaCard.jsx
@@ -1,10 +1,14 @@
 import './TeaCard.css';
+import Leaves from '../../assets/tobacco-icon.svg'
 
 function TeaCard({ id, title, description, temperature, brew_time }) {
 
     return (
         <section className="tea_card">
-            <h4>{title}</h4>
+            <div className="head">
+                <img src={Leaves}/>
+                <h4>{title}</h4>
+            </div>
             <div className="tea_info"> 
                 <p>{description}</p>
                 <p>Brew at {Math.trunc(temperature)}{"\u00B0"}F for {brew_time}</p>
